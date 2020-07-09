26a4f4ee87f60a852af70331870f17d03859d684
Colin Jaffe <balloonasaurus@gmail.com>
2020-07-09 09:23:07 -0400

    Adds alternate solution.

--- emagify.js
+++ emagify.js
@@ -5,6 +5,21 @@ const words = require('./emagi-words');
 const emagify = function(userWords) {
   const results = [];
   for (const word of userWords) {
+    const wordIndex = words.indexOf(word);
+    if (wordIndex === -1) {
+      results.push(word)
+    } else {
+      results.push(symbols[wordIndex]);
+    }
+  }
+
+  return results;
+}
+
+// uses includes and no intermediate variables
+const emagifyAlt = function(userWords) {
+  const results = [];
+  for (const word of userWords) {
     if (words.includes(word)) {
       results.push(symbols[words.indexOf(word)]);
     } else {
@@ -14,6 +29,5 @@ const emagify = function(userWords) {
 
   return results;
 }
-
 
 module.exports = emagify;
